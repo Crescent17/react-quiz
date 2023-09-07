60befbea94b93df33e74ec154a75cde9a0882552
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-07 18:35:41 +0300

    add points for the correct answer

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const initialState = {
     status: 'loading',
     index: 0,
     answer: null,
-    
+    points: 0
 }
 
 function reducer(state, action) {
@@ -25,7 +25,8 @@ function reducer(state, action) {
         case 'start':
             return {...state, status: 'active'}
         case 'newAnswer':
-            return {...state, answer: action.payload}
+            const question = state.questions.at(state.index)
+            return {...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points}
         default:
             throw new Error('Action is unknown')
     }
