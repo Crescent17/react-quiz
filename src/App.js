aa193f429b155b1546a199e759830d042715c238
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-07 16:12:37 +0300

    render page depending on status, destructure initial state object, pass numQuestions prop into StartScreen component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Main from "./Main";
 import {useEffect, useReducer} from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
     questions: [],
@@ -22,7 +25,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+
+    const numQuestions = questions.length
 
     useEffect(() => {
         async function getData() {
@@ -42,8 +47,9 @@ export default function App() {
         <div className='app'>
             <Header/>
             <Main>
-                <p>1/15</p>
-                <p>Question</p>
+                {status === 'loading' && <Loader/>}
+                {status === 'error' && <Error/>}
+                {status === 'ready' && <StartScreen numQuestions={numQuestions}/>}
             </Main>
         </div>
     );
